1ba89617386db58b92549c66769ef9da8822d19a
Chardaviski <eduardaleal753@gmail.com>
2023-08-16 13:23:22 -0300

    Regras para declaracao de variaveis

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,5 +2,22 @@ public class MinhaClasse {
 
     public static void main (String [] args){
         System.out.print("Olá mundo");
+
+        /*Quando queremos dizer que
+        a variável na verdade será
+        uma constante nós nomeamos
+        ela com todas as letras maiúsculas
+         */
+        String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIRO = 27;
+        int ANO_2000 = 2000;
+
+        /*Jamais começar uma variável
+        com números,
+        com caracteres diferentes de _ e $,
+        não declarar uma variável contendo espaço no nome,
+        e não usar palavras reservadas da linguagem
+         */
     }
 }
