e435038a6228ae812b520e48e5af3d21ee0d2890
Chardaviski <eduardaleal753@gmail.com>
2023-08-23 15:40:05 -0300

    Exemplo de MSN para entender conceitos de POO

---
+++ src/msnMensager/Computador.java
@@ -0,0 +1,9 @@
+package msnMensager;
+
+public class Computador {
+    public static void main(String[] args) {
+        MSNMensager msn = new MSNMensager();
+
+        msn.enviarMensagem();
+    }
+}
